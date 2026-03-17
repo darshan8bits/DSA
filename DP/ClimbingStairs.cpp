6fe90f28e2c6ac58fa8d64be790c9f444e2fa77c
Darshan Gupta <darshangupta78421@gmail.com>
2026-03-17 22:21:35 +0530

    Add files via upload
    dp

---
+++ DP/ClimbingStairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int climbStairs(int n) {
+        if(n <= 1) return n;
+        int prev2 = 0, prev = 1;
+        for(int i = 2; i <= n + 1; i++){
+            int curr = prev2 + prev;
+            prev2 = prev;
+            prev = curr;
+        }
+        return prev;
+    }
+}
