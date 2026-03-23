ec04b58acaace94230a7e345203e30c3fe3b311b
Darshan Gupta <darshangupta78421@gmail.com>
2026-03-23 23:39:41 +0530

    Add files via upload
    Neetcode 150

---
+++ NeetCode 150/Two Sum.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> mpp;
+        for(int i = 0; i < nums.size(); i++){
+            int req = target - nums[i];
+            if(mpp.count(req)) return {mpp[req], i};
+            mpp[nums[i]] = i;
+        }
+        return {};
+    }
+};
