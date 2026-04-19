3aeddb1c1e61af8aa7ba3760c24ef25a2c89c96b
Darshan Gupta <darshangupta78421@gmail.com>
2026-04-19 12:03:36 +0530

    Add files via upload

---
+++ Random/Vowel Game.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool doesAliceWin(string s) {
+        int count = 0;
+        for(auto c : s){
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 
+            'u') count++;
+        }
+        return (count == 0) ? false : true;
+    }
+};
